e6cf50eb5c6c99f565fbbe5dcccf13d0a43aafa7
lasyareddy110906 <24eg105f61@anurag.edu.in>
2026-05-22 15:19:13 +0530

    Update Home.jsx

--- employee-frontend/src/components/Home.jsx
+++ employee-frontend/src/components/Home.jsx
@@ -1,21 +1,28 @@
-import {useContext} from 'react'
-import {counterContextObj} from '../contexts/ContextProvider'
+import { useContext } from 'react'
+import { counterContextObj } from '../contexts/ContextProvider'
 import Test from './Test'
 import { useCounterStore } from '../stores/useCounterStore'
 
 function Home() {
-  const newCounter = useCounterStore((state)=>state.newCounter)
-  const incrementCounter = useCounterStore((state)=>state.incrementCounter)
+  const newCounter = useCounterStore((state) => state.newCounter)
 
   console.log("home")
+
   return (
-    <div >
+    <div className="bg-pink-300 min-h-screen flex flex-col items-center justify-center">
       
-      <h1 className="text-2xl font-bold">Counter Home: {newCounter}</h1>
-      <button onClick={incrementCounter} className="bg-blue-500 text-white px-4 py-2 rounded gap-1.5">
+      <h1 className="text-2xl font-bold">
+        Counter Home: {newCounter}
+      </h1>
+
+      <button
+        onClick={incrementCounter}
+        className="bg-blue-500 text-white px-4 py-2 rounded gap-1.5 mt-4"
+      >
         Increment
       </button>
-     <Test/>
+
+      <Test />
        
     </div>
   )
